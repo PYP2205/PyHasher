4cefdf824e5927a1ee6bfe972be016fe4959b617
Paramon, Yevstigneyev <90145033+ParamonPlay2205@users.noreply.github.com>
2022-07-17 09:59:15 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,36 @@
 """
-PyEncryptor
+PyHasher
 
 Programmed by: Paramon Yevstigneyev
 Programmed in: Python 3.8.10 (64-Bit)
 
 Description:
-This is a Python program used for encrypting a string
-into any encryption algorithm you select.
+This is a Python program used for hashing and salting a string
+into any hashing algorithm you select.
 """
 
 
 def main():
-    import encrypt
+    import hash
     import argparse
     import os
 
 
     parser = argparse.ArgumentParser()
 	# Sets a descriptiption of the program, when the user enters "--help" or "-h" it will display the decription.
-    parser.description = "Securly encrypt your strings with (almost) any encryption algorithm."
+    parser.description = "Securly hash your strings with (almost) any hashion algorithm."
 	# Stores the string that will be hashed.
-    parser.add_argument("--string","--string", help="String to encrypt.")
+    parser.add_argument("--string","--string", help="String to hash.")
 	# Stores the algorithm name specifed by the user, to hash the string.
-    parser.add_argument("--algorithm", "--algorithm", help="Encryption algorithm to use to encrypt the string.")
+    parser.add_argument("--algorithm", "--algorithm", help="Hashing algorithm to use to hash the string.")
     # Stores the salt to add to the hashed string.
     parser.add_argument("--salt", "--salt", help="If specifed, this will make your hashed string (possibly) more secure [Recommended]")
 	# Lists all algorithms that can be used to hash a string.
-    parser.add_argument("--list-algorithms", "--list_algorithms", help="List all encryption algorithms to encrypt your string.", action="store_true")
-	# Used for hashing the user's string with all (currently supported) encryption algorithms.
-    parser.add_argument("--use-all-algorithms", "--use_all_algorithms", help="Encrypts your string with all (currently) supported encyrption algorithms.", action="store_true")
-	# Writes the user's string(s), hashing algorithm, and encrypted string into a .txt file.
-    parser.add_argument("--write-to-file", "--write_to_file", help="[Recommended] When called, it will store the string and encrypted string(s) into a .txt file.")
+    parser.add_argument("--list-algorithms", "--list_algorithms", help="List all hashing algorithms to hash your string.", action="store_true")
+	# Used for hashing the user's string with all (currently supported) hashion algorithms.
+    parser.add_argument("--use-all-algorithms", "--use_all_algorithms", help="Hashes your string with all (currently) supported hashing algorithms.", action="store_true")
+	# Writes the user's string(s), hashing algorithm, and hashed string into a .txt file.
+    parser.add_argument("--write-to-file", "--write_to_file", help="[Recommended] When called, it will store the string and hashed string(s) into a .txt file.")
     args = parser.parse_args()
     
     algorithm = str(args.algorithm)
@@ -51,27 +51,27 @@ def main():
     else:
         write_hash_to_file = False
     
-	# Used for encrypting string(s) into all supported encryption algorithms.
-    def encrypt_string_with_all_algorithms(string, salt):
+	# Used for hashing string(s) into all supported hashion algorithms.
+    def hash_string_with_all_algorithms(string, salt):
         # Makes the hashes global, to prevent exceptions from being raised when the user wants the hashes in a .txt file.
         global md5_hash, salted_md5_hash, sha1_hash, salted_sha1_hash, sha224_hash, salted_sha224_hash, sha256_hash, salted_sha256_hash, sha384_hash, salted_sha384_hash, sha512_hash, salted_sha512_hash
         
         # Creates the hashes and shows them to the user's console.
         if hash_salted:
-            md5_hash, salted_md5_hash = encryptor.md5(string, salt)
-            sha1_hash, salted_sha1_hash = encryptor.sha1(string, salt)
-            sha224_hash, salted_sha224_hash = encryptor.sha224(string, salt)
-            sha256_hash, salted_sha256_hash = encryptor.sha256(string, salt)
-            sha384_hash, salted_sha384_hash = encryptor.sha384(string, salt)
-            sha512_hash, salted_sha512_hash = encryptor.sha512(string, salt)
+            md5_hash, salted_md5_hash = hasher.md5(string, salt)
+            sha1_hash, salted_sha1_hash = hasher.sha1(string, salt)
+            sha224_hash, salted_sha224_hash = hasher.sha224(string, salt)
+            sha256_hash, salted_sha256_hash = hasher.sha256(string, salt)
+            sha384_hash, salted_sha384_hash = hasher.sha384(string, salt)
+            sha512_hash, salted_sha512_hash = hasher.sha512(string, salt)
             print(f"\nString: {string}\n\nSalt: {salt}\n\nMD5 value: {md5_hash}\n\nSalted MD5 value: {salted_md5_hash}\n\nSHA-1 value: {sha1_hash}\n\nSalted SHA-1 value: {salted_sha1_hash}\n\nSHA-224 value: {sha224_hash}\n\nSalted SHA-224 value: {salted_sha224_hash}\n\nSHA-256 value: {sha256_hash}\n\nSalted SHA-256: {salted_sha256_hash}\n\nSHA-384 value: {sha384_hash}\n\nSalted SHA-384 value: {salted_sha384_hash}\n\nSHA-512 value: {sha512_hash}\n\nSalted SHA-512 value: {salted_sha512_hash}\n")
         else:
-            md5_hash = encryptor.md5(string, salt=None)
-            sha1_hash = encryptor.sha1(string, salt=None)
-            sha224_hash = encryptor.sha224(string, salt=None)
-            sha256_hash = encryptor.sha256(string, salt=None)
-            sha384_hash = encryptor.sha384(string, salt=None)
-            sha512_hash = encryptor.sha512(string, salt=None)
+            md5_hash = hasher.md5(string, salt=None)
+            sha1_hash = hasher.sha1(string, salt=None)
+            sha224_hash = hasher.sha224(string, salt=None)
+            sha256_hash = hasher.sha256(string, salt=None)
+            sha384_hash = hasher.sha384(string, salt=None)
+            sha512_hash = hasher.sha512(string, salt=None)
             print(f"\nString: {string}\n\nMD5 value: {md5_hash}\n\nSHA-1 value: {sha1_hash}\n\nSHA-224 value: {sha224_hash}\n\nSHA-256 value: {sha256_hash}\n\nSHA-384 value: {sha384_hash}\n\nSHA-512 value: {sha512_hash}\n")
 	# Used for writing the user's string(s), hash(es), and hashing algorithm(s).
     def write_to_file():
@@ -104,12 +104,12 @@ def main():
                 break
 
            
-        # If the user enters a valid encryption algorithm, then it will encyrpt the user's string and write the string, hash, and encyrption algorithm to the file.
-        if algorithm.upper() in encryption_algorithms:
+        # If the user enters a valid hashion algorithm, then it will encyrpt the user's string and write the string, hash, and encyrption algorithm to the file.
+        if algorithm.upper() in hashion_algorithms:
             if algorithm.upper() == "MD5":
                 
                 if hash_salted:
-                    hashed_string, salted_hash = encryptor.md5(string, salt)
+                    hashed_string, salted_hash = hasher.md5(string, salt)
                     file_stream.write(
 f"""String: {string}
 Salt: {salt}
@@ -118,7 +118,7 @@ Salted MD5 value: {salted_hash}
 
 """)
                 else:
-                    hashed_string = encryptor.md5(string, salt=None)
+                    hashed_string = hasher.md5(string, salt=None)
                     file_stream.write(
 f"""String: {string}
 MD5 value: {hashed_string}
@@ -128,7 +128,7 @@ MD5 value: {hashed_string}
 
             elif algorithm.upper() == "SHA1":
                 if hash_salted:
-                    hashed_string, salted_hash = encryptor.sha1(string, salt)
+                    hashed_string, salted_hash = hasher.sha1(string, salt)
                     file_stream.write(
 f"""String: {string}
 Salt: {salt}
@@ -137,7 +137,7 @@ Salted SHA-1 value: {salted_hash}
 
 """)
                 else:
-                    hashed_string = encryptor.sha1(string, salt=None)
+                    hashed_string = hasher.sha1(string, salt=None)
                     file_stream.write(
 f"""String: {string}
 SHA-1 value: {hashed_string}
@@ -147,7 +147,7 @@ SHA-1 value: {hashed_string}
 
             elif algorithm.upper() == "SHA224":
                 if hash_salted:
-                    hashed_string, salted_hash = encryptor.sha224(string, salt)
+                    hashed_string, salted_hash = hasher.sha224(string, salt)
                     file_stream.write(
 f"""String: {string}
 Salt: {salt}
@@ -156,7 +156,7 @@ Salted SHA-224 value: {salted_hash}
 
 """)
                 else:
-                    hashed_string = encryptor.sha224(string, salt=None)
+                    hashed_string = hasher.sha224(string, salt=None)
                     file_stream.write(
 f"""String: {string}
 SHA-224 value: {hashed_string}
@@ -166,7 +166,7 @@ SHA-224 value: {hashed_string}
 
             elif algorithm.upper() == "SHA256":
                 if hash_salted:
-                    hashed_string, salted_hash = encryptor.sha256(string, salt)
+                    hashed_string, salted_hash = hasher.sha256(string, salt)
                     file_stream.write(
 f"""String: {string}
 Salt: {salt}
@@ -175,7 +175,7 @@ Salted SHA-256 value: {salted_hash}
 
 """)
                 else:
-                    hashed_string = encryptor.sha256(string, salt=None)
+                    hashed_string = hasher.sha256(string, salt=None)
                     file_stream.write(
 f"""String: {string}
 SHA-256 value: {hashed_string}
@@ -185,7 +185,7 @@ SHA-256 value: {hashed_string}
 
             elif algorithm.upper() == "SHA384":
                 if hash_salted:
-                    hashed_string, salted_hash = encryptor.sha384(string, salt)
+                    hashed_string, salted_hash = hasher.sha384(string, salt)
                     file_stream.write(
 f"""String: {string}
 Salt: {salt}
@@ -194,7 +194,7 @@ Salted SHA-384 value: {salted_hash}
 
 """)
                 else:
-                    hashed_string = encryptor.sha384(string, salt=None)
+                    hashed_string = hasher.sha384(string, salt=None)
                     file_stream.write(
 f"""String: {string}
 SHA-384 value: {hashed_string}
@@ -204,7 +204,7 @@ SHA-384 value: {hashed_string}
 
             elif algorithm.upper() == "SHA512":
                 if hash_salted:
-                    hashed_string, salted_hash = encryptor.sha512(string, salt)
+                    hashed_string, salted_hash = hasher.sha512(string, salt)
                     file_stream.write(
     f"""String: {string}
     Salt: {salt}
@@ -213,7 +213,7 @@ SHA-384 value: {hashed_string}
 
     """)
                 else:
-                    hashed_string = encryptor.sha512(string, salt=None)
+                    hashed_string = hasher.sha512(string, salt=None)
                 file_stream.write(
 f"""String: {string}
 SHA-512 value: {hashed_string}
@@ -263,100 +263,100 @@ SHA-512 value: {sha512_hash}
         else:
             pass
 
-	# Used for encrypting a user's string, and displaying the string, hash, and algorithm used.
-    def encrypt_string():
+	# Used for hashing a user's string, and displaying the string, hash, and algorithm used.
+    def hash_string():
 
-        # If the user enters a valid encryption algorithm, then it will encyrpt the string and show the string and hash on the console window.
-        if algorithm.upper() in encryption_algorithms:
+        # If the user enters a valid hashion algorithm, then it will encyrpt the string and show the string and hash on the console window.
+        if algorithm.upper() in hashion_algorithms:
             if algorithm.upper() == "MD5":
                 if hash_salted:
-                    hashed_string, salted_hash = encryptor.md5(string, salt)
+                    hashed_string, salted_hash = hasher.md5(string, salt)
                     print(f"\nString: {string}\nSalt: {salt}\n\n{algorithm.upper()} value: {hashed_string}\nSalted {algorithm.upper()} value: {salted_hash}\n")
                     
                 else:
-                    hashed_string = encryptor.md5(string, salt=None)
+                    hashed_string = hasher.md5(string, salt=None)
                     print(f"\nString: {string}\n{algorithm.upper()} value: {hashed_string}\n")
                     
             elif algorithm.upper() == "SHA1":
                 if hash_salted:
-                    hashed_string, salted_hash = encryptor.sha1(string, salt)
+                    hashed_string, salted_hash = hasher.sha1(string, salt)
                     print(f"\nString: {string}\nSalt: {salt}\n\n{algorithm.upper()} value: {hashed_string}\nSalted {algorithm.upper()} value: {salted_hash}\n")
 
                 else:
-                    hashed_string = encryptor.sha1(string, salt=None)
+                    hashed_string = hasher.sha1(string, salt=None)
                     print(f"\nString: {string}\n{algorithm.upper()} value: {hashed_string}\n")
             
             elif algorithm.upper() == "SHA224":
                 if hash_salted:
-                    hashed_string, salted_hash = encryptor.sha224(string, salt)
+                    hashed_string, salted_hash = hasher.sha224(string, salt)
                     print(f"\nString: {string}\nSalt: {salt}\n\n{algorithm.upper()} value: {hashed_string}\nSalted {algorithm.upper()} value: {salted_hash}\n")
 
                 else:
-                    hashed_string = encryptor.sha224(string, salt=None)
+                    hashed_string = hasher.sha224(string, salt=None)
                     print(f"\nString: {string}\n{algorithm.upper()} value: {hashed_string}\n")
             
             elif algorithm.upper() == "SHA256":
                 if hash_salted:
-                    hashed_string, salted_hash = encryptor.sha256(string, salt)
+                    hashed_string, salted_hash = hasher.sha256(string, salt)
                     print(f"\nString: {string}\nSalt: {salt}\n\n{algorithm.upper()} value: {hashed_string}\nSalted {algorithm.upper()} value: {salted_hash}\n")
 
                 else:
-                    hashed_string = encryptor.sha256(string, salt=None)
+                    hashed_string = hasher.sha256(string, salt=None)
                     print(f"\nString: {string}\n{algorithm.upper()} value: {hashed_string}\n")
 
             elif algorithm.upper() == "SHA384":
                 if hash_salted:
-                    hashed_string, salted_hash = encryptor.sha384(string, salt)
+                    hashed_string, salted_hash = hasher.sha384(string, salt)
                     print(f"\nString: {string}\nSalt: {salt}\n\n{algorithm.upper()} value: {hashed_string}\nSalted {algorithm.upper()} value: {salted_hash}\n")
 
                 else:
-                    hashed_string = encryptor.sha384(string, salt=None)
+                    hashed_string = hasher.sha384(string, salt=None)
                     print(f"\nString: {string}\n{algorithm.upper()} value: {hashed_string}\n")
 
             elif algorithm.upper() == "SHA512":
                 if hash_salted:
-                    hashed_string, salted_hash = encryptor.sha512(string, salt)
+                    hashed_string, salted_hash = hasher.sha512(string, salt)
                     print(f"\nString: {string}\nSalt: {salt}\n\n{algorithm.upper()} value: {hashed_string}\nSalted {algorithm.upper()} value: {salted_hash}\n")
 
                 else:
-                    hashed_string = encryptor.sha512(string, salt=None)
+                    hashed_string = hasher.sha512(string, salt=None)
                     print(f"\nString: {string}\n{algorithm.upper()} value: {hashed_string}\n")
         else:
             pass
 	
 	# Stores (currently supported) names of hashing algorithms, which is used to list out what algorithms can be used.
-    encryption_algorithms = ["MD5", "SHA1", "SHA224", "SHA256","SHA384", "SHA512"]
+    hash_algorithms = ["MD5", "SHA1", "SHA224", "SHA256","SHA384", "SHA512"]
 	
-	# Creates encryptor, for encrypting string(s).
-    encryptor = encrypt.user_string()
+	# Creates hasher, for hashing string(s).
+    hasher = hash.user_string()
     if args.list_algorithms:
-        for algorithm in encryption_algorithms:
+        for algorithm in hash_algorithms:
             print(algorithm)
         print()
 
     elif args.use_all_algorithms:
         if write_hash_to_file:
             if hash_salted:
-                encrypt_string_with_all_algorithms(string, salt)
+                hash_string_with_all_algorithms(string, salt)
                 write_to_file()
             else:
-                encrypt_string_with_all_algorithms(string, salt=None)
+                hash_string_with_all_algorithms(string, salt=None)
                 write_to_file()
 
         elif not write_hash_to_file:
             if hash_salted:
-                encrypt_string_with_all_algorithms(string, salt)
+                hash_string_with_all_algorithms(string, salt)
             else:
-                encrypt_string_with_all_algorithms(string, salt=None)
+                hash_string_with_all_algorithms(string, salt=None)
 
     # If the user wants to have the hash and string saved into a file, then it will display the hash and string on the user's console. And it will have it writen into a seperate file.
     elif write_hash_to_file:
-        encrypt_string()
+        hash_string()
         write_to_file()
 
-    # If the user does not want the string and hash saved into a file, then it will only show the string and encrypted hash onto the user's console.
+    # If the user does not want the string and hash saved into a file, then it will only show the string and hashed hash onto the user's console.
     elif not write_hash_to_file:
-        encrypt_string()
+        hash_string()
         
     else:
         pass
